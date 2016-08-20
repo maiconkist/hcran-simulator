d88389b4fdd9ea8bb767af954e979b9f5cb27d02
Matias Schimuneck <schimuneck.matias@gmail.com>
2016-08-20 10:11:59 -0300

    MC New tests

--- src/energy_efficient_scenario.py
+++ src/energy_efficient_scenario.py
@@ -464,7 +464,7 @@ if __name__ == "__main__":
 
 #    ues = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
     #ues = [15]
-    ues = [60]
+    ues = [13, 30, 60]
 
     num_cores = multiprocessing.cpu_count()
     for nues in ues:
