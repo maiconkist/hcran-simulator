5151d09e66a379acd292f616095c0aa0d3f40cdb
Matias Schimuneck <schimuneck.matias@gmail.com>
2016-10-21 22:30:31 -0200

    mc

--- src/icc-experiment.sh
+++ src/icc-experiment.sh
@@ -1,10 +1,10 @@
-MAXUE=100
-STEPUE=10
+MAXUE=30
+STEPUE=30
 MAXREP=5
 STEPREP=1
  
 for r in `seq 0 $STEPREP $(($MAXREP - 1))`; do
-	for u in `seq 10 $STEPUE $(($MAXUE))`; do
+	for u in `seq 30 $STEPUE $(($MAXUE))`; do
 		python icc.py $r $u &
 	done    
 done
